817fa8558e1f13f2fc788b3ee1685dd1921d7d8a
Murddy <thomas.lienard2@gmail.com>
2025-12-02 22:05:18 +0100

    part 5.2

--- pokedex/pokemon/handler_html.go
+++ pokedex/pokemon/handler_html.go
@@ -5,11 +5,13 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func RegisterHTMLRoutes(r *gin.Engine) {
 	r.GET("/pokemons", listPokemonsHTML)
 	r.GET("/pokemons/new", newPokemonFormHTML)
+	r.POST("/pokemons", createPokemonHTML)
 	r.GET("/pokemons/:id", pokemonDetailHTML)
 }
 
@@ -90,4 +92,39 @@ func newPokemonFormHTML(c *gin.Context) {
 		"errors": []string{},
 		"input":  CreatePokemonInput{},
 	})
+}
+
+func createPokemonHTML(c *gin.Context) {
+	var input CreatePokemonInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		var messages []string
+		if verrs, ok := err.(validator.ValidationErrors); ok {
+			for _, fe := range verrs {
+				switch fe.Field() {
+				case "Name":
+					messages = append(messages, "Le nom est obligatoire et max 50 caractères.")
+				case "Types":
+					messages = append(messages, "Types invalides (ex : Fire, Water, Grass).")
+				case "BaseExperience":
+					messages = append(messages, "L'expérience de base est obligatoire et doit être comprise entre 1 et 1000.")
+				case "Weight":
+					messages = append(messages, "Le poids est obligatoire et doit être compris entre 1 et 10000.")
+				case "Height":
+					messages = append(messages, "La taille est obligatoire et doit être comprise entre 1 et 100.")
+				case "Stats":
+					messages = append(messages, "Les statistiques sont obligatoires et doivent être valides.")
+				case "Sprites":
+					messages = append(messages, "Les sprites sont obligatoires et doivent être valides.")
+				default:
+					messages = append(messages, fe.Field()+" invalide.")
+				}
+			}
+		}
+		RespondError(c, 400, messages)
+		return
+	}
+
+	p := Create(input)
+	RespondCreated(c, p)
+
 }
